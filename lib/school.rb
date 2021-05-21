af57971b9c42eb013d9fcbf7f4e37397828fde27
Zach Frohardt <zfrohardt@gmail.com>
2021-05-20 18:13:37 -0700

    commit tree 80a3d68d2806cd21195851c3b299ce766d36b5e5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
+require 'pry'
 # code here!
+class School
+    attr_reader :name
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] = @roster[grade].push(name)
+    end
+
+    def grade(g)
+        @roster[g]
+    end
+    
+    def sort
+        @roster.each_key do |key|
+            @roster[key].sort!
+        end
+    end
+
+end
